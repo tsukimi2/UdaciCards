e1877d13ac026106bcebf9c62d4f532ee911cb11
osiris <osiris@osiriss-MacBook-Pro.local>
2017-11-07 10:56:58 +0800

    Modified App.js and installed redux-subscriber

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import Expo from "expo"
+import MyRoot from './components/MyRoot'
 
-export default class App extends React.Component {
+export default class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			is_ready: false
+		}
+	}
+
+	async componentWillMount() {
+		await Expo.Font.loadAsync({
+			Roboto: require("native-base/Fonts/Roboto.ttf"),
+			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+			Ionicons: require("native-base/Fonts/Ionicons.ttf")			
+		})
+		this.setState({ is_ready: true })
+	}
+
+	render() {
+		if(!this.state.is_ready) {
+			return <Expo.AppLoading />
+		}
+
+		return <MyRoot />
+	}
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <MyRoot />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
